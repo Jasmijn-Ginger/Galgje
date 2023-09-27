53df26047f00d7df9c1105ece1322148ee0b155b
Jasmijn-Ginger <jasmijn.h@live.nl>
2023-09-27 13:27:28 +0200

    HelloWorld weggegooid toegevoegd: converting wordToBeGuessed into array Changed the formatting to visualisedWordToBeGuessed Cleaned up format

--- src/Main.java
+++ src/Main.java
@@ -2,73 +2,85 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
+import java.lang.*;
 
 public class Main {
     public static void main(String[] args) {
-        HelloWorld.printHelloworld();
 
         //creating a new Galgje Word: wordToBeGuessed
         String wordToBeGuessed = CreateGalgjeWord.listOfWords[CreateGalgjeWord.random_int].toUpperCase();
         System.out.println(wordToBeGuessed);
         System.out.println("The word you need to guess is " + wordToBeGuessed.length() + " letters long.");
 
+        //converting the wordToBeGuessed into an array: arrayWordToBeGuessed
+        char[] arrayWordToBeGuessed = new char[wordToBeGuessed.length()];
+        for (int i = 0; i < wordToBeGuessed.length(); i++) {
+            arrayWordToBeGuessed[i] = wordToBeGuessed.charAt(i);
+        }
+//        Alleen van toepassing bij printen
+//        for (char letterWordToBeGuessed : arrayWordToBeGuessed) {
+//            System.out.println(letterWordToBeGuessed);
+//        }
+
         //Visualising the word for the player
         String[] strArray = {};
         List<String> visualisedWordToBeGuessed = new ArrayList<>(Arrays.asList(strArray));
         for (int i = 0; i < wordToBeGuessed.length(); i++) {
-            visualisedWordToBeGuessed.add(" [   ] ");
+            visualisedWordToBeGuessed.add("_ ");
         }
-        for(String val : visualisedWordToBeGuessed) {
+        for (String val : visualisedWordToBeGuessed) {
             System.out.print(val);
         }
 
 
         //number of tries
         int numberOfTriesLeft = 10;
-        System.out.println("You have " + numberOfTriesLeft + " tries left.");
+        System.out.println("\nYou have " + numberOfTriesLeft + " tries left.");
 
         //Player chooses letter: chosenLetter
         Scanner ChooseALetter = new Scanner(System.in);
         System.out.print("Which letter do you want to guess? ");
         String chosenLetter = ChooseALetter.next().toUpperCase();
 
-
         //chosenLetter is checked to see if it's valid
         // Later doen door Swing instelling Formatted Text Field
 
-
-        //chosenLetter is checked to see if it's in the wordToBeGuessed
-        /*
-        Loop door de letters heen van wordToBeGuessed,
-        check of die gelijk zijn met chosenLetter
-        sla de index daarvan op
-        geef een boodschap weer
-        vervang de lege plekken op progressWordToBeGuessed met de chosenLetter
-        doe numberOfTriesLeft - 1
-         */
+        //String chosenLetter converting to char
+        char charChosenLetter = chosenLetter.charAt(0);
+        System.out.println("You choose letter: " + charChosenLetter);
 
 
         //Checking if the letter is in the wordToBeGuessed, and displaying message
         boolean LetterInWord = wordToBeGuessed.contains(chosenLetter);
         if (LetterInWord) {
             System.out.println("The letter you chose is in the word :)");
-            int index = wordToBeGuessed.indexOf(chosenLetter);
-            while (index != -1) {
-                System.out.println(index);
-                index = wordToBeGuessed.indexOf(chosenLetter, index + 1);
-            }
-        }
-        else {
+        } else {
             System.out.println("The letter you chose is not in the word :(");
         }
 
+            /*
+            If letter in word:
+            go through letters in arrayWordToBeGuessed
+            compare to chosenLetter
+            safe index
+            replace same index of visualisedWordToBeGuessed with ChosenLetter
+            go on with loop
+            do numberOfTriesLeft -1
+             */
+
+//        for (char letterWordToBeGuessed : arrayWordToBeGuessed) {
+//            if (letterWordToBeGuessed.equals(charChosenLetter)){
+//
+//            }
+//        }
 
 
-
+//        //Displaying the guessed letter in visualisedWordToBeGuessed
 
 
     }
 }
+
 
 /*
 
